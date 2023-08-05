94c867a73832295a5546714bc3154c321e01e4c7
paularafaat <paula@example.com>
2023-08-04 17:03:58 -0700

    d

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,18 +6,22 @@
  * @argv:pionter to array of arguments.
  * Return: 0 (Success), 1 (Error)
  */
-int main(int argc, char **argv)
-{
-	int i;
 
-	if (argc == 3)
-	{
-		i = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", i);
-	}
-	else
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
 }
