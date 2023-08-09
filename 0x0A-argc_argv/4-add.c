216f77f1563205cba077b6026a0aebd771db1aa6
paularafaat <paula@example.com>
2023-08-09 15:19:54 -0700

    d

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print the name of the program
+ * @argc: the number of arguments
+ * @argv: array of pionters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int p = 0;
+	char *c;
+
+	while (argc--)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < 48 || *c > '9')
+				return (printf("Error\n"), 1);
+		p += atio(argv[argc]);
+	}
+	printf("%d\n", p);
+	return (0);
+}
