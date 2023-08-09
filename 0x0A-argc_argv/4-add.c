accd3b326232e7cc6068e0a26d68c42153715a5a
paularafaat <paula@example.com>
2023-08-09 15:26:19 -0700

    d

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	while (argc--)
 	{
 		for (c = argv[argc]; *c; c++)
-			if (*c < 48 || *c > '9')
+			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
 		p += atoi(argv[argc]);
 	}
