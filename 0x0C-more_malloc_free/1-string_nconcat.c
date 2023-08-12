103ec78efa80e8101043343037fc010530b50b1c
paularafaat <paula@example.com>
2023-08-11 18:36:19 -0700

    d

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,9 +17,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (length1 = 0; s1[lenght1] != '\0'; length1++)
+	for (length1 = 0; s1[length1] != '\0'; length1++)
 		;
-	for (length2 = 0; s2[lemgth2] != '\0'; length2++)
+	for (length2 = 0; s2[length2] != '\0'; length2++)
 		;
 
 	p = malloc(length1 + n + 1);
