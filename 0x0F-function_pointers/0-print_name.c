4ade24a6a41008a28650e7407d7de0a44c242e92
paularafaat <paula@example.com>
2023-08-18 08:38:36 -0700

    d

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - printing input string
+ * @name: string
+ * @f: pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
