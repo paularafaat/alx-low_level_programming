6effd49ec8a919ed81139eb307bec06f89ce30c5
paularafaat <paula@example.com>
2023-08-17 17:05:38 -0700

    f

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - fun outputs the sum of all numbers
+ * @n: int
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int p = 0;
+	int i = n;
+	va_list sum;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(sum, n);
+	for (i = 0; i < n; i--)
+		p += va_arg(sum, int);
+	va_end(sum);
+	return (p);
+}
+
