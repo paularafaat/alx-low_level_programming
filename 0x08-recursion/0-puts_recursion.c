b9dc8153fee38003a46cca00904d13c85eeb5dcc
paula raafat <paula.com>
2023-09-28 15:47:42 -0700

    printing a string with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,10 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n')
-	}
-	else
-	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_puts_recursion(s + 1);
 	}
 
 }
