f8c16d0dc5bc452501917dd87923e5f9a0277717
paula <paula>
2023-10-03 05:34:41 +0300

    program to multi tow nums

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,11 +13,11 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 		x = atoi(argv[1]) * atoi(argv[2]);
-		pritnf("%d\n", x);
+		printf("%d\n", x);
 	}
 	else
 	{
-		pritnf("Error\n");
+		printf("Error\n");
 		return (1);
 	}
 	return (0);
