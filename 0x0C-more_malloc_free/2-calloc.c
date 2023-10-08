7860c3fcf0f939ad216d8a42b3ffae928bd29233
paula <paula>
2023-10-08 23:27:38 +0300

    malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _memset - set memory wiht constan byte
+ * @z: pointer
+ * @p: string
+ * @c: int
+ * Return: nothing
+ */
+char *_memset(char *z, char p, unsigned int c)
+{
+	char *ptr = s;
+
+	while (c--)
+		*z++ = p;
+	return (ptr);
+}
+
+
+
+/**
+ * *_calloc - function that allocates memory for array
+ * @nmemb: integer
+ * @size: the size of array
+ * Return: nothing
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *x;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	m = malloc(sizeof(int) * nmemb);
+	if (x == 0)
+	return (NULL);
+	return (x);
+}
