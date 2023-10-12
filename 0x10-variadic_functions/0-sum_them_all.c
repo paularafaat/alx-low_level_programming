5f40ba7d73811f3afa8fdfb973b055686cfa30da
paula <paula>
2023-10-13 02:51:01 +0300

    function to sum all nums

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,11 +6,11 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0, i;
+	unsigned int sum = 0, i;
+	va_list args;
 
 	if (n == 0)
 		return (0);
-	va_list args;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
