f1a3073ef5f92e9537afd6bddf21a76655581571
paula <paula.com>
2023-10-27 03:25:52 +0200

    d

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_binary - fun to print binary
+ * @n: integer
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	int b = sizeof(n) * 8, p = 0;
+
+	while (b)
+	{
+		if (n & 1l << --b)
+		{
+			_putchar('1');
+			p++;
+		}
+		else if (p)
+			_putchar('0');
+	}
+	if (!p)
+		_putchar('0');
+
+}
