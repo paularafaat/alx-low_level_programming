bcff98d4011f9a24a23ba830285a07e96e06ad7c
paula <paula.com>
2023-11-30 22:58:21 +0200

    appending text to the end of file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -32,11 +32,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
+	if (text_content != NULL)
+	{
 	ws = write(fd, text_content, _strlen(text_content));
 	if (ws == -1)
 	{
 		close(fd);
 		return (-1);
+	}
 	}
 	close(fd);
 		return (1);
