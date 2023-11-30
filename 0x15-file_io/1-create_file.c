fbf3e9a48d79bc9889240e21fb6932c59160cba5
paula <paula.com>
2023-11-30 22:13:56 +0200

    creating file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,17 +9,19 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, Ws;
 
-	fd = fopen(filename, 0_WEONLY | 0_CREAT | 0_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_contnet != NULL)
+	{
 		Ws = write(fd, text_content, strlen(text_content));
 	if (Ws == -1)
 	{
 		close(fd);
 		return (-1);
 	}
-	cloes(fd);
+	}
+	close(fd);
 	return (1);
 
 }
